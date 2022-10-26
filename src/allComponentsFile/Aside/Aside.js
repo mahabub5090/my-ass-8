3e0ae0ad3f3a905310b852dd9360076b01229255
mahabub5090 <mdmahabub.rahman789@gmail.com>
2022-10-27 02:28:41 +0600

    all UI done

--- src/allComponentsFile/Aside/Aside.js
+++ src/allComponentsFile/Aside/Aside.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import "./Aside.css";
 
-const Aside = () => {
+const Aside = () =>
+{
     return (
-        <div>
-            <h1>aside</h1>
+        <div className='aside'>
+            <div className='author-details'>
+                <div className="Profile-name">
+                    <h3>Md.Mahabubar Rahman</h3>
+                    <p><small>From: Rangpur, Bangladesh</small></p>
+                </div>
+                <div className="author-information">
+                    <div className="author-info">
+                        <h3>60 KG</h3>
+                        <h4>Weight</h4>
+                    </div>
+                    <div className="author-info">
+                        <h3>18 Year's</h3>
+                        <h4>Age</h4>
+                    </div>
+                    <div className="author-info">
+                        <h3>5.05</h3>
+                        <h4>Height</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div className=''></div>
+
+           
+                <div className='exercise-management'>
+                    <h3>Your's Exercise Details:</h3>
+                    <div className='ex-details'>
+                        <h4>Exercise Time</h4>
+                        <h4> seconds</h4>
+                    </div>
+                    <div className='ex-details'>
+                        <h4>Break Time</h4>
+                        <h4> seconds</h4>
+                    </div>
+                    <button className="completed-btn">Activity Completed</button>
+
+                </div>
+            
+
         </div>
+
     );
 };
 
